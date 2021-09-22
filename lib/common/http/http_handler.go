868e4fa48073272c78de3e20617e1e88ec696064
Dwi Kurnianto Mulyadien <dwikurnianto.mulyadien@gmail.com>
2021-09-22 11:54:15 +0700

    Add swagger & config

--- lib/common/http/http_handler.go
+++ lib/common/http/http_handler.go
@@ -5,25 +5,31 @@ import (
 	"net/http"
 	"time"
 
+	config "github.com/Meruya-Technology/go-boilerplate/lib/common/config"
 	"github.com/gorilla/mux"
 )
 
 type HttpHandler struct {
 	router mux.Router
+	config config.Config
 }
 
-func (x HttpHandler) Initialize(z mux.Router) {
+func (x HttpHandler) Initialize(z mux.Router, c config.Config) {
 	x.router = z
+	x.config = c
 	x.StartAndListen()
 }
 
 func (x HttpHandler) StartAndListen() {
+	Config := x.config
+	Host := Config.Host
+	Port := Config.Port
+	Address := Host + ":" + Port
 	srv := &http.Server{
 		Handler:      &x.router,
-		Addr:         "127.0.0.1:8000",
+		Addr:         Address,
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
 	}
-
 	log.Fatal(srv.ListenAndServe())
 }
--- main.go
+++ main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"github.com/Meruya-Technology/go-boilerplate/common/http"
-	"github.com/Meruya-Technology/go-boilerplate/common/router"
+	"github.com/Meruya-Technology/go-boilerplate/lib/common/config"
+	"github.com/Meruya-Technology/go-boilerplate/lib/common/http"
+	"github.com/Meruya-Technology/go-boilerplate/lib/common/router"
 )
 
 func main() {
@@ -12,9 +13,53 @@ func main() {
 	/// Handle router
 	routeHandler := router.Handle()
 
+	/// Initialize config
+	config := new(config.Config)
+
 	/// Initialize Http handler
 	http := new(http.HttpHandler)
 
 	/// Bind router to HttpHandler
-	http.Initialize(*routeHandler)
+	http.Initialize(*routeHandler, config.LoadConfig())
 }
+
+// @title Swagger Example API
+// @version 1.0
+// @description This is a sample server celler server.
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host localhost:8080
+// @BasePath /api/v1
+// @securityDefinitions.basic BasicAuth
+
+// @securityDefinitions.apikey ApiKeyAuth
+// @in header
+// @name Authorization
+
+// @securitydefinitions.oauth2.application OAuth2Application
+// @tokenUrl https://example.com/oauth/token
+// @scope.write Grants write access
+// @scope.admin Grants read and write access to administrative information
+
+// @securitydefinitions.oauth2.implicit OAuth2Implicit
+// @authorizationUrl https://example.com/oauth/authorize
+// @scope.write Grants write access
+// @scope.admin Grants read and write access to administrative information
+
+// @securitydefinitions.oauth2.password OAuth2Password
+// @tokenUrl https://example.com/oauth/token
+// @scope.read Grants read access
+// @scope.write Grants write access
+// @scope.admin Grants read and write access to administrative information
+
+// @securitydefinitions.oauth2.accessCode OAuth2AccessCode
+// @tokenUrl https://example.com/oauth/token
+// @authorizationUrl https://example.com/oauth/authorize
+// @scope.admin Grants read and write access to administrative information
