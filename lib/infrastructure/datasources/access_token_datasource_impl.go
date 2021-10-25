64cc05ba3c7e7208d8f3808ee08a4c48dc6ca9ba
Dwi Kurnianto Mulyadien <dwikurnianto.mulyadien@gmail.com>
2021-10-25 11:33:41 +0700

    Add more datasources

--- lib/infrastructure/datasources/access_token_datasource_impl.go
+++ lib/infrastructure/datasources/access_token_datasource_impl.go
@@ -1,3 +1,17 @@
 package datasources
 
-type AccessTokenDatasourceImpl struct{}
+import (
+	ctx "context"
+	"database/sql"
+
+	cfg "github.com/Meruya-Technology/go-boilerplate/lib/common/config"
+)
+
+type AccessTokenDatasourceImpl struct {
+	Config        cfg.Config
+	DBTransaction *sql.Tx
+}
+
+func (a AccessTokenDatasourceImpl) Create(ctx ctx.Context) {
+
+}
